2aea40941ad94057b3457a649df4a23229e145e6
HUAL <48791485+huaasc@users.noreply.github.com>
2022-05-06 01:09:09 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'huallll',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '',
 
   // social usernames (optional)
   twitter: '',
